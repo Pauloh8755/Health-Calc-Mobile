b71ada3641b23d85e0d6526e69a3912760566875
PAULO HENRIQUE DE ANDRADE JUNIOR <21193420@CFP127ALUNO.LOCAL>
2021-10-04 16:54:56 -0300

    DeashBoard Completa

---
+++ app/src/main/java/com/example/healthcalc/DashBoardActivity.kt
@@ -0,0 +1,13 @@
+package com.example.healthcalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashBoardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dash_board)
+
+        supportActionBar!!.hide()
+    }
+}
--- app/src/main/java/com/example/healthcalc/MainActivity.kt
+++ app/src/main/java/com/example/healthcalc/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.healthcalc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar!!.hide()
+
+        //Criando instancia do botão entrar
+        val buttonEntrar = findViewById<Button>(R.id.btn_entrar)
+        buttonEntrar.setOnClickListener{
+            val abrirDashBoard = Intent(this, DashBoardActivity::class.java)
+            startActivity(abrirDashBoard)
+        }
     }
 }
